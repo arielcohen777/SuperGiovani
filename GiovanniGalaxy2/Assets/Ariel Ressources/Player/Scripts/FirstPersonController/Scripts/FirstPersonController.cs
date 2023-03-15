9a4d036de447914fba13dad1f3dd179cbdd8ac19
arielcohen777 <ariel.m.cocohen@gmail.com>
2023-03-15 14:14:00 -0400

    Fix for gun bug

--- GiovanniGalaxy2/Assets/Ariel Ressources/Player/Scripts/FirstPersonController/Scripts/FirstPersonController.cs
+++ GiovanniGalaxy2/Assets/Ariel Ressources/Player/Scripts/FirstPersonController/Scripts/FirstPersonController.cs
@@ -193,9 +193,7 @@ namespace StarterAssets
 				}
 			}
 				
-			}
-		
-		
+		}
 
 		private void Move()
 		{
@@ -259,6 +257,9 @@ namespace StarterAssets
 				// move
 				inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
 			}
+			else
+				gm.shoot.anim.SetBool("Sprinting", false);
+
 
 			// move the player
 			_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
